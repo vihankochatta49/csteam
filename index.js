c0395611792a9c6d0ed90f5af85fe3ed9db301d2
Vihan <vihankochatta49@gmail.com>
2022-01-27 14:40:17 +0530

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/team.html"));
+});
+
+app.get("/present-team", (req, res) => {
+  res.sendFile(path.join(__dirname, "/presentTeam.html"));
+});
+
+app.get("/past-teams", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pastTeam.html"));
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
